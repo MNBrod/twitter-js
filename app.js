b4cff17e813289570cf636274c1a80ef8881e645
Max Brodheim <max.brodheim@gmail.com>
2017-06-12 18:33:29 -0400

    end of class

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-
+app.use((req, res, next) => {
+  console.log(res.status(400));
+  next();
+})
 app.get('/', (req, res) => {
-  res.end('Hello!');
+  //res.end('Hello!');
+  res.status(200).end('ended');
 });
