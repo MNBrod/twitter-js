cee09f9dcebcfa0621ff8b0cf0bab1e25f23fbc3
Max Brodheim <max.brodheim@gmail.com>
2017-06-13 10:01:04 -0400

    special middleware up

--- app.js
+++ app.js
@@ -12,10 +12,17 @@ app.listen(PORT, () => {
  * Middleware
  */
 app.use((req, res, next) => {
-  console.log('Request Type:', req.method)
-  console.log('Request URL:', req.originalUrl)
+  console.log('Request Type:', req.method);
+  console.log('Request URL:', req.originalUrl);
   next();
-})
+});
+app.use('/special/', (req, res, next) => {
+  console.log('Special middleware');
+  next();
+});
+app.get('/special/', (req, res, next) => {
+  res.status(200).end('you reached special');
+});
 app.get('/', (req, res) => {
   //res.end('Hello!');
   res.status(200).end('ended');
