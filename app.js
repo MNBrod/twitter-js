bb3306e5876673eec6a8bf7a2cd5504b1650240a
Max Brodheim <max.brodheim@gmail.com>
2017-06-13 10:54:32 -0400

    got the template working

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const PORT = 3000;
+const nunjucks = require('nunjucks');
 
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views', {
+  noCache: true
+}); // point nunjucks to the proper directory for templates
+
 /**
  * Middleware
  */
@@ -23,7 +31,24 @@ app.use('/special/', (req, res, next) => {
 app.get('/special/', (req, res, next) => {
   res.status(200).end('you reached special');
 });
+
 app.get('/', (req, res) => {
-  //res.end('Hello!');
-  res.status(200).end('ended');
+  res.status(200);
+  res.render('index', locals);
+  //res.end();
 });
+
+
+var locals = {
+  title: 'An example',
+  people: [
+    { name: 'Gandlaf the Grey' },
+    { name: 'Frodo the not Grey' },
+    { name: 'Hermione the out-of-place' }
+  ]
+};
+
+// nunjucks.render('index.html', locals, (err, out) => {
+//   console.log(out);
+// });
+
